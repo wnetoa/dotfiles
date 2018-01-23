86e458ff24b0b0dc5c6c121d5918b8973b500bf6
Martin Tomanec <code+github@myspot.at>
2018-01-23 01:36:11 +0100

    history-fix

--- .oh-my-zsh/lib/history.zsh
+++ .oh-my-zsh/lib/history.zsh
@@ -8,10 +8,14 @@ SAVEHIST=20000
 
 # Show history
 case $HIST_STAMPS in
-  "mm/dd/yyyy") alias history='fc -fl 1' ;;
-  "dd.mm.yyyy") alias history='fc -El 1' ;;
-  "yyyy-mm-dd") alias history='fc -il 1' ;;
-  *) alias history='fc -l 1' ;;
+#  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+#  "dd.mm.yyyy") alias history='fc -El 1' ;;
+#  "yyyy-mm-dd") alias history='fc -il 1' ;;
+#  *) alias history='fc -l 1' ;;
+  "mm/dd/yyyy") alias history='fc -fl' ;;
+  "dd.mm.yyyy") alias history='fc -El' ;;
+  "yyyy-mm-dd") alias history='fc -il' ;;
+  *) alias history='fc -l' ;;
 esac
 
 setopt append_history
